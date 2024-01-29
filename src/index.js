9786094b7d6eaa167f311b759a14d3e41b2084df
SIRAPHONG <seanta000@gmail.com>
2024-01-29 11:20:38 +0700

    test123

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
+app.get("/books", (req, res) => {
   res.send("Hello world! SIRAPHONG Check!!");
 });
 
